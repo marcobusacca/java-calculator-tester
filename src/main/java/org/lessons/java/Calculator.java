c01232c2312826fade24498311e7a3e756e46992
marcobusacca <marcobusacca01@gmail.com>
2023-11-27 13:02:15 +0100

    init "Calculator" and "CalculatorTest"

--- src/main/java/org/lessons/java/Calculator.java
+++ src/main/java/org/lessons/java/Calculator.java
@@ -2,4 +2,24 @@ package org.lessons.java;
 
 public class Calculator {
 
+	public float add(float num1, float num2) {
+		
+		return num1 + num2;
+	}
+	public float subtract(float num1, float num2) {
+		
+		return num1 - num2;
+	}
+	public float divide(float num1, float num2) throws Exception {
+		
+		if (num2 == 0) {
+			throw new Exception("non puoi dividere per zero");
+		}
+		
+		return num1 / num2;
+	}
+	public float multiply(float num1, float num2) {
+		
+		return num1 * num2;
+	}
 }
--- src/test/java/org/lessons/java/CalculatorTest.java
+++ src/test/java/org/lessons/java/CalculatorTest.java
@@ -1,5 +1,31 @@
 package org.lessons.java;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
 public class CalculatorTest {
 
+    @BeforeAll
+    public static void setup() {
+        // eseguito una volta
+        // prima di tutti i metodi di test
+    }
+    
+    @BeforeEach
+    public void init() {
+        // eseguito prima di ogni metodo di test
+    }
+    
+    @AfterEach
+    public void clear() {
+        // eseguito dopo ogni metodo di test
+    }
+    
+    @AfterAll
+    public static void cleanup() {
+        // eseguito una volta
+        // dopo di tutti i metodi di test
+    }
 }
