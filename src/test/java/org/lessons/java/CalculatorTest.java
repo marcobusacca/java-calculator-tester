2dc33c300cb2833021bd65059fbece74dfaae267
marcobusacca <marcobusacca01@gmail.com>
2023-11-27 14:46:54 +0100

    Esercizio Completato

--- src/test/java/org/lessons/java/CalculatorTest.java
+++ src/test/java/org/lessons/java/CalculatorTest.java
@@ -1,11 +1,21 @@
 package org.lessons.java;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
+	
+	float num1 = 5;
+	float num2 = 5;
+
+	Calculator c = new Calculator();
 
     @BeforeAll
     public static void setup() {
@@ -27,5 +37,43 @@ public class CalculatorTest {
     public static void cleanup() {
         // eseguito una volta
         // dopo di tutti i metodi di test
+    }
+    
+    @Test
+    public void addTest() {
+    	
+    		assertEquals(num1 + num2, c.add(num1, num2), "Somma non riuscita");
+    }
+    
+    @Test
+    public void subtractTest() {
+    	
+    		assertEquals(num1 - num2, c.subtract(num1, num2), "Sottrazione non riuscita");
+    }
+    
+    @Test
+    public void divideTest() throws Exception {
+    	
+    		assertEquals(num1 / num2, c.divide(num1, num2), "Divisione non riuscita");
+    }
+    
+    @Test
+    public void multiplyTest() throws Exception {
+    	
+    		assertEquals(num1 * num2, c.multiply(num1, num2), "Moltiplicazione non riuscita");
+    }
+    
+    @Test
+    public void goodDivideNumbersTest() {
+    	
+    		assertDoesNotThrow(() -> c.divide(num1, num2), "Eccezione generata");
+    }
+    
+    @Test
+    public void badDivideNumbersTest() {
+    	
+    		assertThrows(Exception.class,
+				() -> c.divide(num1, 0),
+				"Non è stata lanciata un'eccezione");
     }
 }
